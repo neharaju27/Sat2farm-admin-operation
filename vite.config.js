208c90370d0284a9e69b82881b5cf8f746db82f1
prateektripathi619 <prateektripathi.sae.comp@gmail.com>
2026-03-23 15:10:29 +0530

    Feat : Making Changes in Vite.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "./", // 🔥 VERY IMPORTANT (fixes blank page)
+
   plugins: [react()],
+
   server: {
     proxy: {
-      '/data': {
-        target: 'https://api.sat2farm.com',
+      "/data": {
+        target: "https://api.sat2farm.com",
         changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-})
+        secure: false,
+      },
+    },
+  },
+});
