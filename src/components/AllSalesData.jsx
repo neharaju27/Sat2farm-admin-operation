eb5df98e9c9b300f9053377d007a0f7a792e2d54
satkumar <sat@satyukt.com>
2026-08-27 10:54:35 +0530

    Update all salesdata  dashboard

--- src/components/AllSalesData.jsx
+++ src/components/AllSalesData.jsx
@@ -64,7 +64,7 @@ export default function AllSalesData({ user, onPageChange }) {
       // List of sales users
       const salesUsers = [
         'Nirosha', 'Aymen','Chaturya', 'Priyanshu', 'Bhagwati', 'Harshitha', 'Shurti', 'Vijay K B',
-        'Mustaqeem', 'Akhil Kumar M','Amith', 'Rohini','Ashok', 'Lipsa', 'Pragya', 'Alisha', 
+        'Mustaqeem', 'Akhil Kumar M','Amith', 'Rohini','Ashok', 'Lipsa', 'Pragya', 'Alisha','Shyamli', 'Rohith S',
       ];
 
       const salesDataPromises = salesUsers.map(async (salesName) => {
